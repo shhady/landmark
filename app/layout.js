bdf78a4f07dec05a0b83b35f48d3fbeb23a56b9d
shhady <shhadyse@gmail.com>
2026-02-10 15:54:40 +0200

    TEst

--- app/layout.js
+++ app/layout.js
@@ -16,20 +16,39 @@ const rubik = Rubik({
 })
 
 export const metadata = {
-  title: 'שירותי מדידה מקצועיים | Landmap-Ltd',
+  title: 'Landmap | שירותי הנדסה ומדידות',
   description: 'חברת מדידות מקצועית המספקת שירותי מדידה מתקדמים בצפון. התמחות בתצ"ר, היתרי בנייה ומיפוי פוטוגרמטרי.',
+  icons: {
+    icon: [
+      { url: '/favicon.ico' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+    ],
+    shortcut: '/favicon.ico',
+    apple: [
+      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+    ],
+  },
+  openGraph: {
+    title: 'Landmap | שירותי הנדסה ומדידות',
+    description: 'חברת מדידות מקצועית המספקת שירותי מדידה מתקדמים בצפון. התמחות בתצ"ר, היתרי בנייה ומיפוי פוטוגרמטרי.',
+    locale: "he_IL",
+    type: "website",
+    siteName: 'Landmap',
+    images: [
+      {
+        url: '/logo1200x600.png',
+        width: 1200,
+        height: 600,
+        alt: 'Landmap Logo',
+      },
+    ],
+  },
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="he" dir="rtl" className={`${heebo.variable} ${rubik.variable}`}>
-       <head>
-          <link rel="icon" href="/favicon.ico" sizes="any" />
-          <link rel="icon" href="/favicon-32x32.png" type="image/png" sizes="32x32" />
-          <link rel="icon" href="/favicon-16x16.png" type="image/png" sizes="16x16" />
-          <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-          <link rel="manifest" href="/site.webmanifest" />
-        </head>
       <body className={`overflow-x-hidden font-sans ${heebo.className} text-neutral-800 bg-white`}>
         <Navbar />
         <main className="min-h-screen">{children}</main>
